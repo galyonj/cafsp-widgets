70af18d6f9aa51dc30f44a93fcad82d45ad0188b
galyonj <galyon.jb@gmail.com>
2021-08-26 08:52:28 -0400

    Project directory setup.

---
+++ src/php/cafsp-widgets.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Elementor Pro plugin that creates certain widgets specific to the use case at CAFSP.
+ *
+ * @wordpress-plugin
+ * @package CAFSP_Widgets
+ *
+ * Plugin Name:       CAFSP Widgets
+ * Plugin URI:        https://github.com/galyonj/cafsp-widgets
+ * Description:       Elementor Pro plugin that creates certain widgets specific to the use case at CAFSP.
+ * Version:           1.0.0
+ * Author:            CAFSP
+ * Author URI:        https://github.com/galyonj
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       cafsp-widgets
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
